62f77920e89bfd68439be24c78451fb7a252689e
elujambio <eugeniolujambio@gmail.com>
2016-06-10 21:25:12 -0500

    added footer

--- wp-content/themes/nutgir/footer.php
+++ wp-content/themes/nutgir/footer.php
@@ -3,6 +3,14 @@
     <div class="padder"></div>
 	
     <footer class="blog-footer">
+	    <div class="footer-menu">
+	      <a href="<?php echo esc_url(get_permalink( get_page_by_title( 'Retos' )  ) ); ?>">Retos</a>
+	      <a href="<?php echo esc_url(get_permalink( get_page_by_title( 'Recetas' )  ) ); ?>">Recetas</a>
+	      <a href="<?php echo esc_url(get_permalink( get_page_by_title( 'Tips' )  ) ); ?>">Tips</a>
+	      <a href="<?php echo esc_url(get_permalink( get_page_by_title( 'Programas' )  ) ); ?>">Programas</a>
+	      <label>Nutriologa Gabriela Girón - Nutgir	&copy;</label>
+	      <a href="http://nuva.rocks/">Página por: NUVA.ROCKS</a>
+	    </div>
     </footer>
 
     <?php wp_footer(); ?> 
